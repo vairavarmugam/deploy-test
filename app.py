857b15000b37eb3554234b479a5d5bce8d1e54f6
vairavarumugam <115896465+vairavarmugam@users.noreply.github.com>
2025-08-12 15:21:44 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+import uvicorn
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route('/')
-def hello():
+@app.get("/", response_class=PlainTextResponse)
+async def hello():
     return "Hello world"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=80)
